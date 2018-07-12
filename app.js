7ebe5a4de3bd0a9b796ccac526421a92d70e25cd
zhao xin ke <zhaoxinke@boco.com.cn>
2018-07-12 12:53:01 +0800

    添加router和parseBody方法调整工程目录结构

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const Koa = require('koa2');
-
+const router = require('koa-router')();
+const koaBody = require('koa-body');
 const app = new Koa();
+
+app.use(router.routes());
 
 app.use(async (ctx, next) => {
   console.info(`${ctx.request.method} ${ctx.request.url}`);
   await next();
 });
-app.use(async (ctx, next) => {
-    const start = new Date().getTime();
-    await next();
-    const ms = new Date().getTime() - start;
-    console.info(`耗时：${ms}ms`);
-});
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/heml';
-    ctx.response.body = `<h1>${ctx.request.url}</h1>`
+
+router.post('/login', koaBody(), async(ctx, next) => {
+    console.log('login', ctx.request.body);
 })
+
+app.use(async (ctx, next) => {
+  const start = new Date().getTime();
+  await next();
+  const ms = new Date().getTime() - start;
+  console.info(`耗时：${ms}ms`);
+});
+
+app.use(async (ctx, next) => {
+  await next();
+  ctx.response.type = 'text/heml';
+  ctx.response.body = `<h1>${ctx.request.url}</h1>`;
+});
 
 app.listen(3000);
